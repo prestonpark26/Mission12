f5081a34a4ea25ae7097e8a8c00fe66a5468ef2b
prestonpark26 <153689632+prestonpark26@users.noreply.github.com>
2025-04-02 15:46:19 -0600

    fix category link

--- frontend/src/components/CategoryFilter.tsx
+++ frontend/src/components/CategoryFilter.tsx
@@ -14,7 +14,7 @@ function CategoryFilter({
     const fetchCategories = async () => {
       try {
         const response = await fetch(
-          'https://localhost:5000/book/GetBookCategories'
+          'https://mission13-park-backend.azurewebsites.net/book/GetBookCategories'
         );
         const data = await response.json();
         console.log('Fetched categories:', data);
